5ab544a293516124dd72807380718c53b69cb23b
Andy Young <acespify@gmail.com>
2025-03-27 20:25:25 -0700

    added a notes card component

---
+++ src/app/components/notes-card/notes-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IonCard } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-notes-card',
+  templateUrl: './notes-card.component.html',
+  styleUrls: ['./notes-card.component.scss'],
+  standalone: false
+})
+export class NotesCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
+import { NotesCardComponent } from 'src/app/components/notes-card/notes-card.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,6 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, NotesCardComponent]
 })
 export class HomePageModule {}
