fff32fb75b274bf93126ef98e766a7f98ed08f8b
Andy Young <acespify@gmail.com>
2025-04-02 17:38:24 -0700

    Error Component is functional now.

--- src/app/components/error-message/error-message.component.spec.ts
+++ src/app/components/error-message/error-message.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ErrorMessageComponent } from './error-message.component';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 
 describe('ErrorMessageComponent', () => {
   let component: ErrorMessageComponent;
@@ -18,7 +19,42 @@ describe('ErrorMessageComponent', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should show error message on field touched and error is present', () =>{
+    component.field = new FormGroup({anyField: new FormControl()});
+
+    component.field.markAsTouched();
+    component.field.setErrors({anyError: true});
+    component.error = "anyError";
+
+    expect(component.shouldShowComponent()).toBeTruthy();
+  })
+  
+  it('should hide the error message on field not touched', () => {
+
+    component.field = new FormGroup({anyField: new FormControl()});
+
+    component.field.setErrors({anyError: true});
+    component.error = "anyError";
+
+    expect(component.shouldShowComponent()).toBeFalsy();
+  })
+
+  it('should hide the error message on thr field touched, but has no errors', () => {
+    component.field = new FormGroup({anyField: new FormControl()});
+
+    component.field.markAsTouched();
+    component.error = "anyError";
+
+    expect(component.shouldShowComponent()).toBeFalsy();
+  })
+
+  it('should hide the error message on field touched and has error, but it is a different error', () => {
+    component.field = new FormGroup({anyField: new FormControl()});
+
+    component.field.markAsTouched();
+    component.field.setErrors({anyError: true});
+    component.error = "anotherError";
+
+    expect(component.shouldShowComponent()).toBeFalsy();
+  })
 });
--- src/app/components/error-message/error-message.component.ts
+++ src/app/components/error-message/error-message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-error-message',
@@ -9,9 +10,22 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ErrorMessageComponent  implements OnInit {
 
   @Input() message!: string;
+  @Input() field!: AbstractControl;
+  @Input() error!: string;
 
   constructor() { }
 
   ngOnInit() {}
+
+  shouldShowComponent() {
+    //form.get('email').touched && form.get('email').errors?.required
+    if(this.field === undefined)
+      return false;
+
+    if(this.field?.touched && this.field.errors?.[this.error]) {
+      return true;
+    }
+    return false;
+  }
 
 }
